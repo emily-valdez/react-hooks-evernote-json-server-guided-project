ef524b90afccf54ab38b45982cc669bab47dbd10
hayleerees <rae8504679@gmail.com>
2023-10-16 19:29:44 -0400

    fetch done in NoteContainer and passed to Sidebar

--- src/components/NoteContainer.js
+++ src/components/NoteContainer.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Search from "./Search";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
 
 function NoteContainer() {
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/notes')
+    .then(r => r.json())
+    .then((allNotes) => setNotes(allNotes))
+  }, [])
   return (
     <>
       <Search />
       <div className="container">
-        <Sidebar />
+        <Sidebar notes={notes}/>
         <Content />
       </div>
     </>
