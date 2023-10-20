6dd8a48fba869b533f7e844a9285a7b5719cd4d9
Emily Valdez <emily.milan.valdez@gmail.com>
2023-10-20 08:51:00 -0500

    edit deliverable complete

--- src/components/NoteEditor.js
+++ src/components/NoteEditor.js
@@ -2,8 +2,8 @@ import React, {useState} from "react";
 
 function NoteEditor({viewNote, handleEditor}) {
 
-  const [title, setTitle] = useState()
-  const [body, setBody] = useState()
+  const [title, setTitle] = useState(viewNote.title)
+  const [body, setBody] = useState(viewNote.body)
 
   function handleTitleChange(e) {
     setTitle(e.target.value)
@@ -13,10 +13,12 @@ function NoteEditor({viewNote, handleEditor}) {
     setBody(e.target.value)
   }
 
+  
+
   return (
     <form className="note-editor">
-      <input type="text" name="title" value={viewNote.title} onChange={handleTitleChange}/>
-      <textarea name="body" value={viewNote.body} onChange={handleBodyChange}/>
+      <input type="text" name="title" value={title} onChange={handleTitleChange}/>
+      <textarea name="body" value={body} onChange={handleBodyChange}/>
       <div className="button-row">
         <input className="button" type="submit" value="Save"/>
         <button type="button">Cancel</button>
